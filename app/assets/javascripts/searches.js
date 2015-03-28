26e624f643a6635cceee7e76a97026209ab5455c
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-03-28 17:46:43 -0400

    WORKING

---
+++ app/assets/javascripts/searches.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  submitListener();
+
+})
+
+function submitListener(){
+  $('#new_search').on("submit", submitSearch)
+}
+
+function submitSearch(){
+  
+}
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,9 +10,11 @@ class SearchesController < ApplicationController
 
   def create
 
-    @result = Search.new.am_i_ruby(search_params)
-    
-    render :results
+    @result = Search.new.am_i_ruby(search_params)    
+    respond_to do |f|
+      f.html
+      f.js 
+    end
   end
 
   private
